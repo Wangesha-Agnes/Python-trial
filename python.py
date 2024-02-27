ebe97d0e269e9c920d3fc3a7234040c024892d8f
Student <wangeshaagnes@gmail.com>
2024-02-27 07:47:46 +0300

    python calculation

--- python.py
+++ python.py
@@ -1,5 +1,14 @@
-x = input("Type a number: ")
+x = input("Enter number ")
 y = input("Type another number: ")
 
 Division = int(x) / int (y)
 print("The division is: ", Division)
+
+multiplication = int(x) * int(y)
+print("The multiplication is: ", multiplication)
+
+Addition = int(x) + int(y)
+print("The sum: ", Addition)
+
+subtraction = int(x) - int(y)
+print("The subtraction is: ", subtraction)
